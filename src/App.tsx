f044dad4e515a18fb242d2bf604373b01da9183f
jk <jklagrosa.dev@gmail.com>
2022-08-23 19:12:14 +0800

    Added components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
+import Navigation from "./components/Navigation";
+
 function App() {
   return (
     <>
-      Id duis ad ullamco culpa ex. Ipsum cillum adipisicing eiusmod anim minim
-      et ad consequat velit in aliquip. Adipisicing exercitation officia
-      proident irure incididunt aliqua cillum. Sint minim ex eu ipsum ea ex
-      pariatur sunt ullamco consequat tempor ex. Laborum minim pariatur qui
-      cillum exercitation sunt quis culpa amet nisi.
+      <Navigation />
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./public/index.html",
+    "./src/pages/**/*.{html,js}",
+    "./src/components/**/*.{html,js}",
+    "./src/layouts/**/*.{html,js}",
+    "./src/index.html",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
