3d72d92d1b8dd27711e1ae11d8d2f2de667527d0
jk <jklagrosa.dev@gmail.com>
2022-09-06 15:55:45 +0800

    Added offcanvas

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,9 +1,13 @@
-import React from "react";
-import { Container, Nav, Navbar } from "react-bootstrap";
+import React, { useState } from "react";
+import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
 import styles from "../styles/scss/Navigation.module.scss";
 import { useLocation, useNavigate } from "react-router-dom";
 
 function Navigation() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -14,7 +18,10 @@ function Navigation() {
           <Navbar.Brand id={styles._navbar_brand_name_}>
             Kusinero<span>Korner</span>
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Toggle
+            aria-controls="basic-navbar-nav"
+            onClick={handleShow}
+          />
           <Navbar.Collapse
             id="basic-navbar-nav"
             className={styles._navbar_collapse_}
@@ -75,6 +82,18 @@ function Navigation() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      {/* OFFCANVAS */}
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+      {/* END */}
     </>
   );
 }
